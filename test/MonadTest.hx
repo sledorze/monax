99f3012945ae098ff0a1e0f6bc187548b921ccb8
sledorze <stephane.ledorze@gmail.com>
2012-01-03 13:42:29 +0100

    added tests

--- test/MonadTest.hx
+++ test/MonadTest.hx
@@ -8,20 +8,35 @@ package ;
 import Standard;
 using Standard;
 
+import massive.munit.Assert;
+// import com.mindrocks.macros.Monad; // just for imorting an definition Option
+
+enum Option<T> {
+  None;
+  Some(x: T);
+}
+
 class MonadTest {
 
-  public static function foo() {
-    
+  public function new() {
   }
-  
-  public static function compilationTest() {
+
+  @Test
+  public function compilationTest() {
     
     var res =
       OptionM.dO({
         value <= ret(55);
-        value1 <= ret(value * 2);        
+        value1 <= ret(value * 2);
         ret(value1 + value);
       });
+  
+    Assert.isTrue(
+      switch (res) {
+        case None: false;
+        case Some(x): x == 165;
+      }
+    );
       
     var res2 = 
       ArrayM.dO({
@@ -30,7 +45,16 @@ class MonadTest {
         c <= ret(1000);
         [a + b + c];
       });
-      
+
+    var expected = [1010, 1020, 1030, 1011, 1021, 1031, 1012, 1022, 1032];
+    var equals = res2.length == expected.length;    
+    if (equals) {
+      for (i in 0...res2.length) {
+        equals = equals && (res2[i] == expected[i]);
+      }
+    }
+    Assert.isTrue(equals);
+    
     var res3 =
       StateM.dO({
         passedState <= gets();
@@ -39,21 +63,20 @@ class MonadTest {
         ret('passed state: '+passedState+' new state: '+state);
       }).runState("1");
 
+    Assert.areEqual(res3, "passed state: 1 new state: 2");
+    
     var dummyReadFile:RC<String,String> = function(cont)  {
       var fileContents = "dummy-content";
       return cont(fileContents);
     }
-
+    
     var res4 =
       ContM.dO({
         headline <= ret(52);
         filecontents <= dummyReadFile;
         ret(Std.string(headline) + "\n" + filecontents);
       })(function(x) return x);
-
-    trace("result " + Std.string(res));
-    trace("result2 " + Std.string(res2));
-    trace("result3 " + Std.string(res3));
-    trace("result4 " + Std.string(res4));
-  }  
+      
+    Assert.areEqual(res4, "52\ndummy-content");
+  }
 }
